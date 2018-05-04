74db4f72d765cf7840491ae23c4f2a1d0011ab72
huotaol <huotao1992@foxmail.com>
2018-05-04 17:56:37 +0800

    修改生成文件后的命令操作

--- app/index.js
+++ app/index.js
@@ -10,20 +10,30 @@ var Reactpackage = yeoman.Base.extend({
             'I am going to build your app!'
         ));
     },
-    generateBasic: function() {  //按照自己的templates目录自定义
-        this.directory('config', 'config');    //拷贝目录
-        this.directory('public', 'public');
-		this.directory('scripts', 'scripts');
-		this.directory('src', 'src');
-        this.copy('package.json', 'package.json');   //拷贝文件
-        this.copy('.env', '.env');
-        this.copy('.eslintrc', '.eslintrc');
-        this.copy('.gitignore', '.gitignore');
+	   writing: {  //生成目录结构阶段
+        app: function () {      //默认源目录就是生成器的templates目录，目标目录就是执行`yo example`时所处的目录。调用this.template用Underscore模板语法去填充模板文件
+            this.directory('config', 'config');    //拷贝目录
+			this.directory('public', 'public');
+			this.directory('scripts', 'scripts');
+			this.directory('src', 'src');
+			this.copy('package.json', 'package.json');   //拷贝文件
+			this.copy('.env', '.env');
+			this.copy('.eslintrc', '.eslintrc');
+			this.copy('.gitignore', '.gitignore');
+        }
     },
-    install: function() {      //安装依赖
-        this.installDependencies({
-            skipInstall: this.options['skip-install']
-        });
+
+    install: function () {
+        var done = this.async();
+        this.spawnCommand('cnpm', ['install'])  //安装项目依赖
+            .on('exit', function (code) {
+                if (code) {
+                    done(new Error('code:' + code));
+                } else {
+                    done();
+                }
+            })
+            .on('error', done);
     },
     end: function() {
         this.log(yosay(
